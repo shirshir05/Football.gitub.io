8b8ab5a5449d0bc3e86d7ed492fd522b150337a2
shirshir05 <shir0@post.bgu.ac.il>
2020-05-22 13:12:58 +0300

    Updates

---
+++ precache-manifest.cef75ad4ab4803e43953ea7c36c7dfeb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee53d43d9cf4edfaf1a394eb97444af7",
+    "url": "/Football.gitub.io/index.html"
+  },
+  {
+    "revision": "0020d1f0ef13b8742f9d",
+    "url": "/Football.gitub.io/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "a68af45eea59a0835b9c",
+    "url": "/Football.gitub.io/static/css/main.d14c77dc.chunk.css"
+  },
+  {
+    "revision": "0020d1f0ef13b8742f9d",
+    "url": "/Football.gitub.io/static/js/2.60a35ab7.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Football.gitub.io/static/js/2.60a35ab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a68af45eea59a0835b9c",
+    "url": "/Football.gitub.io/static/js/main.86255206.chunk.js"
+  },
+  {
+    "revision": "49830865d1938dc54e91",
+    "url": "/Football.gitub.io/static/js/runtime-main.c718cb01.js"
+  },
+  {
+    "revision": "6409091911f35a8fc25df47543b83793",
+    "url": "/Football.gitub.io/static/media/background.64090919.jpg"
+  }
+]);
