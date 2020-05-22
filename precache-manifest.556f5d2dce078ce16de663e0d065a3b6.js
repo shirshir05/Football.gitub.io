1f8a1681074157e9ff5be7059a5053d555587119
shirshir05 <shir0@post.bgu.ac.il>
2020-05-22 13:04:07 +0300

    Updates

---
+++ precache-manifest.556f5d2dce078ce16de663e0d065a3b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c428cc2a35037093fa21e528e13539a",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "0020d1f0ef13b8742f9d",
+    "url": "/login/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "dea451ba908e2cb8a586",
+    "url": "/login/static/css/main.91bd0596.chunk.css"
+  },
+  {
+    "revision": "0020d1f0ef13b8742f9d",
+    "url": "/login/static/js/2.60a35ab7.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/login/static/js/2.60a35ab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea451ba908e2cb8a586",
+    "url": "/login/static/js/main.7408a5b3.chunk.js"
+  },
+  {
+    "revision": "acae061a433c1f6cddef",
+    "url": "/login/static/js/runtime-main.01f66ad1.js"
+  },
+  {
+    "revision": "6409091911f35a8fc25df47543b83793",
+    "url": "/login/static/media/background.64090919.jpg"
+  }
+]);
