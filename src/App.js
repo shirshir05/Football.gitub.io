69ef177bff2339e7033ba96193afbcf25f5d86d6
Ortal Parpara <t-orparp@microsoft.com>
2020-05-24 16:48:22 +0300

    almost

--- src/App.js
+++ src/App.js
@@ -41,8 +41,11 @@ import AnswerComplaints from './components/Complaints/AnswerComplaints'
 import SaveGame from './components/GameManagement/SaveGame'
 import ViewGameEvents from './components/GameManagement/ViewGameEvents'
 import ViewGamesReferee from './components/GameManagement/ViewGamesReferee'
+<<<<<<< HEAD
 import SendComplaint from './components/Complaints/SendComplaint'
 import RegisterToGame from './components/GameManagement/RegisterToGame'
+=======
+>>>>>>> almost
 
 
 function App() {
