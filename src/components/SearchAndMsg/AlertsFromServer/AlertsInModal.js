8a6cc19577439d97b08a6ebcd72333dd15333075
Ortal Parpara <t-orparp@microsoft.com>
2020-05-28 13:51:10 +0300

    display of new alerts

--- src/components/SearchAndMsg/AlertsFromServer/AlertsInModal.js
+++ src/components/SearchAndMsg/AlertsFromServer/AlertsInModal.js
@@ -1,4 +1,4 @@
-import { FaEnvelope } from 'react-icons/fa'
+import { FaEnvelope, FaEnvelopeOpen } from 'react-icons/fa'
 import React, {useState} from 'react'
 import ModalWindow from '../../Modal/ModalWindow'
 import axios from 'axios';
@@ -9,18 +9,37 @@ function AlertsInModal(props) {
     if (props.show === false){
       display = 'none'
     }
+    
     const [alerts, setState] = useState({
       displayAlerts : ['No alerts to display']
     })
-    const [show, setShow] = useState(false);
-    
+
+    const envelopeType = () => {
+      axios.get(API_BASE_URL+sessionStorage.getItem("username")+'/readallalrets', {withCredentials: true })
+        .then(response => {
+          if(response.status === 200){
+            return <FaEnvelope />
+          }
+          else {
+            return <FaEnvelopeOpen/>
+          }
+        })
+        .catch(error => {
+          return <FaEnvelopeOpen/>
+        })
+        return <FaEnvelopeOpen/>
+    }
+
+    console.log(envelopeType())
+
+    const [show, setShow] = useState(false);    
     const handleClose = () => setShow(false);
 
     const handleShow = () => {
         // get alerts from server
-      new Promise((resolved, rejected) => {
+      new Promise(() => {
         try{
-            axios.get(API_BASE_URL+sessionStorage.getItem("username")+'/returnalert', {withCredentials: true })
+            axios.get(API_BASE_URL+sessionStorage.getItem("username")+'/readallalrets', {withCredentials: true })
               .then(response => {
                 if(response.status === 200){
                   console.log(response)
@@ -73,7 +92,7 @@ function AlertsInModal(props) {
     return (
         <>
         <button class="msgBtn" onClick={handleShow} style={{ display: display }}>
-          <FaEnvelope />
+          {envelopeType()}
         </button>
         <ModalWindow show={show} handleClose={handleClose} Header={props.Header} Body={body()}/>
         </>
