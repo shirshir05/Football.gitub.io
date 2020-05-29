8c309b7c6c4b7d7c982a59390f10a70a0660741a
shirshir05 <shir0@post.bgu.ac.il>
2020-05-29 20:07:17 +0300

    teamName.replace(/%20/g)

--- src/components/TeamManagement/ApproveTeamCreation.js
+++ src/components/TeamManagement/ApproveTeamCreation.js
@@ -84,7 +84,7 @@ class ApproveTeamCreation extends Component{
     }
 
     approveTeamInServer(teamName){
-        axios.get(API_BASE_URL+sessionStorage.getItem("username")+'/approveteam/'+ teamName.replace(/ /g, '%20'), {withCredentials: true })
+        axios.get(API_BASE_URL+sessionStorage.getItem("username")+'/approveteam/'+  teamName.replace(/%20/g, " "), {withCredentials: true })
             .then(function (response) {
                 if(response.status === 200){
                     this.setState(prevState => ({
